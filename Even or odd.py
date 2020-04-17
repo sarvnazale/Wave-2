f4d4c7c32a5f8e964cc3221e57eb3499a1b7dc0e
sarvnazale <sarvnazale@gmail.com>
2020-04-17 14:51:38 -0400

    Question 3

--- Even or odd.py
+++ Even or odd.py
@@ -1,5 +1,5 @@
 number = int(input("Enter your number: "))
 if number % 2 == 0:
-    print("Your number is even.")
+    print(number, "is even.")
 else:
-    print("Your number is odd.")
+    print(number, "is odd.")
